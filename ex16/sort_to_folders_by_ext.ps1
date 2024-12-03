32b1173a8e89cccb4b86c05d26be0766a93b413a
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-12-03 17:48:58 +0200

    Sort files to folders by extension

---
+++ ex16/sort_to_folders_by_ext.ps1
@@ -0,0 +1,9 @@
+Get-ChildItem | Where-Object { $_.Extension -ne ".ps1" } | ForEach-Object {
+	$ext = $_.Extension;
+	if (-not (Test-Path $ext)) {
+		New-Item -ItemType Directory -Name $ext;
+	}
+	Move-Item $_ $ext;
+}
+
+# we can trim dot from the extension by using the Substring method $_.Extension.Substring(1)
