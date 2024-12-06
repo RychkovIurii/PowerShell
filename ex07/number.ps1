bbfd59a3d9528ff4d58b8de12d414feaaad692fc
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-12-06 20:36:17 +0200

    Validate number. Day od week.

--- ex07/number.ps1
+++ ex07/number.ps1
@@ -1,11 +1,17 @@
 do {
-	$value = 10;
+	$value = 10.0;
 	$number = Read-Host "Enter a number";
+	if ([string]::IsNullOrWhiteSpace($number)) {
+		Write-Host "Error: Please enter a valid number (int or float)" -ForegroundColor Red;
+		$validInput = $false;
+		continue;
+	}
+
 	try {
-		$number = [long]$number;
+		$number = [double]$number;
 		$validInput = $true;
 	} catch {
-		Write-Host "Error: Please enter a valid number (int) or (long)" -ForegroundColor Red;
+		Write-Host "Error: Please enter a valid number (int or float)" -ForegroundColor Red;
 		$validInput = $false;
 		continue;
 	}
